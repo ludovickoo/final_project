c87258f6ce9509da17fb6b6c26e13c6868b7e700
ludovickoo <ludovic.koo@gmail.com>
2026-09-18 18:13:16 -0400

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):  
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    if response.status_code == 200:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)
+        emotion = formatted_response['emotionPredictions'][0]['emotion']
+        anger_score = emotion["anger"]
+        disgust_score = emotion["disgust"]
+        fear_score = emotion["fear"]
+        joy_score = emotion["joy"]
+        sadness_score = emotion["sadness"]
+        dominant_emotion = max(emotion, key=emotion.get)
+
+    elif response.status_code == 500:
+        emotion = None
+        score = None
+    # For any other unexpected status codes, set label and score to None
+    else:
+        emotion = None
+        score = None
+    return {'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion}    
+        
+    return emotion
