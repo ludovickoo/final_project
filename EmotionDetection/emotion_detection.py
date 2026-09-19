962cb1dd87f3fbb0387e1cb882d1cde5b1dd02d3
Ludovic Koo <ludovic.koo@gmail.com>
2026-09-19 18:51:42 -0400

    task 7 update

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,9 +6,10 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    
+    
     if response.status_code == 200:
-        # Parse the response from the API
-        formatted_response = json.loads(response.text)
         emotion = formatted_response['emotionPredictions'][0]['emotion']
         anger_score = emotion["anger"]
         disgust_score = emotion["disgust"]
@@ -16,19 +17,24 @@ def emotion_detector(text_to_analyse):
         joy_score = emotion["joy"]
         sadness_score = emotion["sadness"]
         dominant_emotion = max(emotion, key=emotion.get)
-
-    elif response.status_code == 500:
-        emotion = None
-        score = None
-    # For any other unexpected status codes, set label and score to None
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
     else:
-        emotion = None
-        score = None
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
+        
     return {'anger': anger_score,
         'disgust': disgust_score,
         'fear': fear_score,
         'joy': joy_score,
         'sadness': sadness_score,
-        'dominant_emotion': dominant_emotion}    
-        
-    return emotion
+        'dominant_emotion': dominant_emotion}
--- server.py
+++ server.py
@@ -11,6 +11,7 @@ def render_index_page():
 def emotionDetector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
+
     anger_score = response["anger"]
     disgust_score = response["disgust"]
     fear_score = response["fear"]
@@ -18,7 +19,10 @@ def emotionDetector():
     sadness_score = response["sadness"]
     dominant_emotion = response["dominant_emotion"]
 
-    return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}"
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+    else:
+        return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}"
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
