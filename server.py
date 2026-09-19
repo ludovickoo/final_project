c14aa003ba83d1ed20293a573feae8345b210d46
Ludovic Koo <ludovic.koo@gmail.com>
2026-09-19 18:23:17 -0400

    update server .py

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("Sentiment Analyzer")
+app = Flask("Emotion Detector")
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emotionDetector():
