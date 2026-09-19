b10e3a7fd94ae801b8573d9229c428329db4d4fb
Ludovic Koo <ludovic.koo@gmail.com>
2026-09-19 19:04:40 -0400

    finished

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+This is the final project for 
+Developing AI Applications with Python and Flask
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +9,16 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """
+        Define '/' route
+    """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def emotion_detect():
+    """
+        Define 'emotionDetector' route
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -21,8 +31,11 @@ def emotionDetector():
 
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}"
+
+    return (f"For the given statement, the system response is 'anger': {anger_score}, "
+        f"'disgust': {disgust_score}, 'fear': {fear_score}, "
+        f"'joy': {joy_score} and 'sadness': {sadness_score}. "
+        f"The dominant emotion is {dominant_emotion}")
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
